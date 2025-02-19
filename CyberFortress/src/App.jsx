2833514816c6899c26e9d301e295883894f33b40
Yakir13 <yakir.helfer100@gmail.com>
2025-02-20 00:27:31 +0200

    Home

--- CyberFortress/src/App.jsx
+++ CyberFortress/src/App.jsx
@@ -1,12 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
 import Navbar from "./components/Navbar/Navbar"
+import Home from "./pages/Home/Home"
 
 function App() {
 
   return (
     <>
     <Navbar/>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/Home' element={<Home/>} />
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
-
-export default App
+export default App;
