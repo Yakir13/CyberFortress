9c16723b7ad5edfccd8ee834c107ba91a0033203
Yakir13 <yakir.helfer100@gmail.com>
2025-02-19 15:07:26 +0200

    first commit

---
+++ Cyber_Website/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import './Navbar.css';
+
+function Navbar(){
+    return(
+        <nav className="nav">
+            <a href="/" className="site_title">CyberWeb</a>
+            <ul>
+                <li><a href="/CD">הגנות סייבר</a></li>
+                <li>
+                    <a href="/CA">התקפות סייבר</a>
+                    <ul className="dropdown">
+                        <li><a href="DoS_DDoS">DDoS and DoS</a></li>
+                        <li><a href="Malware">Malware</a></li>
+                    </ul>
+                </li>
+                <li><a href="/CIS">אבטחת מידע וסייבר</a></li>
+                <li><a href="/Home">עמוד בית</a></li>
+            </ul>
+        </nav>
+    )
+}
+export default Navbar
